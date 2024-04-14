2216cc52f1afab6d506371244535a921f621b900
Gorm Braarvig <gorm_b@hotmail.com>
2024-04-14 14:13:14 +0200

    qwd

---
+++ aniServerBrokerServer/aniServerBrokerServerForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace aniServerBrokerServer
+{
+    partial class aniServerBrokerServerForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(314, 140);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(363, 214);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // aniServerBrokerServerForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "aniServerBrokerServerForm";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ aniServerBrokerServer/aniServerBrokerServerForm.cs
@@ -0,0 +1,18 @@
+namespace aniServerBrokerServer
+{
+    public partial class aniServerBrokerServerForm : Form
+    {
+        public aniServerBrokerServerForm()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
