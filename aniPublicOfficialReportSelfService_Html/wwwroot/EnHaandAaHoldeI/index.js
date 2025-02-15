bbc052849e9bbf6473b53ca02e050c9e2c8b4cf9
Gorm Braarvig <gorm_b@hotmail.com>
2025-02-15 22:47:02 +0100

    qwe

--- aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/index.js
+++ aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/index.js
@@ -1,5 +1,6 @@
 /////////////// Config /////////////////
 const aiPromptWelcome=`Velkommen! Denne chatten svarer på spørsmål du har om oppholdet ditt på CatoSenteret. Hva lurer du på?`
+, aiPromptWelcomeQuestion=`Hva er velkomstmeldingen?`
 , aiPrompt=[{ role: `system`, content: `Du er en empatisk, kunnskapsrik og evidensbasert chatbot som hjelper pasienter som forbereder seg til sitt første opphold på CatoSenteret. 
     Målet ditt er å gi korte konsise tydelige, praktiske og forskningsbaserte råd som støtter pasientenes trygghet, helse og forberedelse. Du skal:
     1. Gi informasjon som er lett å forstå og tilpasset pasientens behov.
@@ -23,16 +24,16 @@ const aiPromptWelcome=`Velkommen! Denne chatten svarer på spørsmål du har om 
     ,[`Hvordan kan jeg forberede meg til oppholdet?`, `God forberedelse kan forbedre rehabiliteringsutbyttet. Sørg for å ha en positiv innstilling og realistiske mål for oppholdet. Dersom du har spørsmål om helsetilstanden din, anbefaler vi å skrive dem ned og ta dem opp med vårt fagteam ved ankomst.`]
     ,[`Hva er 2+2`, `Jeg ønsker ikke å svare på mattespørsmål. Kun spørsmål relevant opphold på Catosenteret`]
     ,[`Hva skjer den første dagen?`, `Den første dagen får du en omvisning og møter teamet ditt. Vi starter med en helhetlig vurdering for å lage en personlig rehabiliteringsplan. Husk at det er normalt å føle seg litt nervøs, men forskning viser at å sette små, oppnåelige mål tidlig gir bedre langsiktig resultat.`]
-    ,['Hva er velkomstmeldingen?', aiPromptWelcome]
+    ,[aiPromptWelcomeQuestion, aiPromptWelcome]
 ]
-, aiConfigDefault='Mistral large'
+, aiConfigDefault='Mistral small'
 , aiConfig = [ // [name, url, gunn, Spørsmålsforslag prompt, Spørsmålsforslag prompt(n), [[aiName, aiModel]]]
     ['Mistral (EU)', 'https://api.mistral.ai/v1/chat/completions', escape('&W%%(`HcWMG](Y[]CEVPz6.CN&#M8]#@'), 'Gi meg et konkret eksempel på neste spørsmål jeg bør stille. Svar kun med spørsmålet, så jeg kan sende dette videre til en annen chat-tjeneste', 'Gi meg enda ett konkret eksempel på neste spørsmål jeg bør stille. Svar kun med spørsmålet, så jeg kan sende dette videre til en annen chat-tjeneste'
-        , [['Mistral large', 'mistral-large-latest'], ['Mistral small§*', 'mistral-small-latest']]]
+        , [['Mistral small', 'mistral-small-latest'], ['Mistral large', 'mistral-large-latest']]]
     ,['Open AI (USA)', 'https://api.openai.com/v1/chat/completions', escape(`4>c/P0p:;X0>]^"4sa1ML)*FtW",*TM]Z#['.CKV"U(PDZOdR!{`), 'Gi meg et konkret eksempel på neste spørsmål jeg bør stille. Svar kun med spørsmålet, så jeg kan sende dette videre til en annen chat-tjeneste', 'Gi meg enda ett konkret eksempel på neste spørsmål jeg bør stille. Svar kun med spørsmålet, så jeg kan sende dette videre til en annen chat-tjeneste'
         , [['GPT 3.5', 'gpt-3.5-turbo'], ['GPT 4', 'gpt-4o-mini'], ['GPT o3', 'o3-mini']]]
     ,['Deepseek (Kina)', 'https://api.deepseek.com/v1/chat/completions', escape('4>c-ueq0~|ye%f}zscw4+wrf%1/zp1tl}/s'), 'Gi meg et konkret eksempel på neste spørsmål jeg bør stille. Svar kun med spørsmålet, så jeg kan sende dette videre til en annen chat-tjeneste', 'Gi meg enda ett konkret eksempel på neste spørsmål jeg bør stille. Svar kun med spørsmålet, så jeg kan sende dette videre til en annen chat-tjeneste'
-        , [['R1', 'R1-model-name'], ['V3', 'v3-model-name']]]]
+        , [['Deepseek chat', 'deepseek-chat'], ['Deepseek reasoner', 'deepseek-reasoner']]]]
 , aiConfigTimeout=10;
 const menuText = `App >>§-
         ||CatoSenteret >>|||Før opphold§*|||Under opphold|||Etter opphold
@@ -44,7 +45,7 @@ const menuText = `App >>§-
         ||Begynn på nytt
         ||Spørsmålsforslag§ *
         ||Grubling
-    |Om >>§-||Kontakt||Personvernerklæring||Barkode||Utvikling >>§-|||Prompt|||Simuler|||Debug`.replace(/(\s*\|)/g, '|').replace(/^\s+|\s+$/g, '')
+    |Om >>§-||Kontakt||Personvernerklæring||Barkode||Utvikling >>§-|||Prompt|||Simuler|||List modeller|||Debug`.replace(/(\s*\|)/g, '|').replace(/^\s+|\s+$/g, '')
 /////////////// ui and Shortcuts //////////////
 const uiChat = document.querySelector('main')
 , uiMenu = document.querySelector('#menu')
@@ -84,8 +85,9 @@ const menuAsArray = mStr => { // create hierarchy from | || ||| string
     m.forEach((mi) => { if (mi.l) m[mi.p].c.push(mi);});
     return m.filter(mi=>!mi.l);
 }
-, menuX= mt => mt.replace(/[^a-zA-Z0-9]/g, '')
+, menuX= mt => mt.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
 , menuId= mt => 'm_'+menuX(mt)
+, menuFn= mt => 'menuClick_' + menuId(mt)
 , menuE = mt => document.getElementById(menuId(mt)) || console.warn(`menuE: ${menuId(mt)} finnes ikke`)
 , menuEBold = (mt,b) => {
     if (b==null) b = menuE(mt).classList.contains('bold');
@@ -113,7 +115,7 @@ const menuAsArray = mStr => { // create hierarchy from | || ||| string
 , menuShow = b => uiShow(uiMenu, b)
 , menuClick_Model=id=>{
     menuEBoldOnly(id, aiConfigAllModels)
-    const c=document.getElementById('m_'+id), d=c.dataset, pd=c.parentElement.dataset;
+    const c=document.getElementById(menuId(id)), d=c.dataset, pd=c.parentElement.dataset;
     aiModel=d.d0;
     aiUrl=pd.d0;
     aiGunnar=unescape(pd.d1);
@@ -122,10 +124,51 @@ const menuAsArray = mStr => { // create hierarchy from | || ||| string
     return 1;
 }
 , menuClick_OpenUrl=u=>window.open(u, '_blank')
-, menuClick_alleSpraak=['Bokml', 'Nynorsk', 'English', 'Svenska', 'Dansk'];
+, menuClick_alleSpraak=['Bokml', 'Nynorsk', 'English', 'Svenska', 'Dansk']
+, menuClick_Models=e=>{
+    menuShow(false);
+    msgAsk('AI-modeller tilgjengelig');
+    let //row=msgAnswer('Sjekker tilbyderne...'),img = row.querySelector('img'), div = row.querySelector('.msg')
+        replys=new Array();
+    aiConfig.forEach((p,n)=>{
+        replys.push('');
+        let r=msgAnswer(p[0]+' modeller...'),i = r.querySelector('img'), d = r.querySelector('.msg'), k=aiGun(unescape(p[2])), u='https://'+new URL(p[1]).hostname + '/models'
+        console.log(u,k);
+        let x=new XMLHttpRequest();
+        x.open('GET',u,true);
+        x.setRequestHeader('Authorization','Bearer '+k);
+        x.setRequestHeader('Accept','application/json');
+        x.onprogress = e => d.innerHTML+=x.responseText;
+        x.onreadystatechange = e => {i.classList.remove('rotating')};
+        x.send();
+    });
+    // let x=new XMLHttpRequest();x.open("GET",aiUrl,true);x.setRequestHeader("Authorization","Bearer "+aiGunn());x.send();
+
+    //img?.classList.remove('rotating');
+    //if (x.status == 200) aiHistory[iThread].push({ role: 'assistant', content: aiReply[iThread] });
+
+    //, aiConfig = [ // [name, url, gunn, Spørsmålsforslag prompt, Spørsmålsforslag prompt(n), [[aiName, aiModel]]]
+
+    // , aiRequest = (q, row = msgAnswer(), iThread = 0, onDone = null, retries = 2) => {
+    //     aiRequestActiveCount++;
+    //     let img = row.querySelector('img'), d = row.querySelector('.msg'), l = 0;
+    //     aiHistory[iThread] ??= [...(aiHistory[aiHistory.length - 1] || [])];
+    //     aiReply[iThread] ??= [...(aiReply[aiReply.length - 1] || [])];
+    //     aiReply[iThread] = d.innerText.replace('↺', '');
+    //     aiHistory[iThread].push({ role: "user", content: q });
+    
+    //     let x = new XMLHttpRequest();
+    //     x.open("POST", aiUrl, true);
+    //     x.setRequestHeader("Content-Type", "application/json");
+    //     x.setRequestHeader("Authorization", "Bearer " + aiGunn());
+    //     x.onprogress = e => l = aiRequestProgress(d, x.responseText, l, iThread);
+    //     x.onreadystatechange = () => x.readyState == 4 && aiRequestComplete(x, img, d, iThread, onDone, retries);
+    //     x.send(JSON.stringify({ model: aiModel, messages: aiHistory[iThread], stream: true }));
+    // };    
+}
 /////////////// menuClick_m_ - Menu handlers ///////////////
-window.menuClick_m_Kommer=e=>menuShow(false)^msgInfo('Under utvikling...')
-window.menuClick_m_Begynnpnytt=e=>{
+window.menuClick_m_kommer=e=>menuShow(false)^msgInfo('Under utvikling...')
+window.menuClick_m_begynnpnytt=e=>{
     uiChat.innerHTML='';
     aiReset();
     msgReset();
@@ -133,17 +176,17 @@ window.menuClick_m_Begynnpnytt=e=>{
 }
 window.menuClick_m_CatoSenteret=(e,sm)=>{ menuShow(); msgInfo(`<i>${sm=='Før opphold'?sm+' er allerede aktivert</i>':sm+' er ikke aktivert'}</i>`);};
 
-window.menuClick_m_=e=>{/* line clicked */};
+window.menuClick_m_=e=>{/* line or blank clicked */};
 window.menuClick_ModelActive=()=>{return 'Mistrallarge'};
-window.menuClick_m_Mistrallarge=e=>menuClick_Model('Mistrallarge');
-window.menuClick_m_Mistralsmall=e=>menuClick_Model('Mistralsmall');
-window.menuClick_m_GPT35=e=>menuClick_Model('GPT35');
-window.menuClick_m_GPT4=e=>menuClick_Model('GPT4');
-window.menuClick_m_GPTo3=e=>menuClick_Model('GPTo3');
-window.menuClick_m_R1=e=>menuClick_Model('R1');
-window.menuClick_m_V3=e=>menuClick_Model('V3');
+window.menuClick_m_mistralsmall=e=>menuClick_Model('mistralsmall');
+window.menuClick_m_mistrallarge=e=>menuClick_Model('mistrallarge');
+window.menuClick_m_gpt35=e=>menuClick_Model('GPT35');
+window.menuClick_m_gpt4=e=>menuClick_Model('GPT4');
+window.menuClick_m_gpto3=e=>menuClick_Model('GPTo3');
+window.menuClick_m_deepseekreasoner=e=>menuClick_Model('deepseek-reasoner');
+window.menuClick_m_deepseekchat=e=>menuClick_Model('deepseek-chat');
 let menuClick_m_ForskalleAC=0;
-window.menuClick_m_ForskalleAI=e=> {
+window.menuClick_m_forskalleai=e=> {
     let m='Gjenta', orgId=menuClick_ModelActive(), cmd='';
     try{  e=uiChat.lastElementChild;
         while (e && !e.classList.contains("sent")) e = e.previousElementSibling;
@@ -154,39 +197,74 @@ window.menuClick_m_ForskalleAI=e=> {
     menuClick_Model(orgId);
 }
 let funcQuestionSuggestion = false, funcDeepAnalysis = false;
-window.menuClick_m_Sprsmlsforslag=e=> {
+window.menuClick_m_sprsmlsforslag=e=> {
     funcQuestionSuggestion = menuEBold('Sprsmlsforslag', !funcQuestionSuggestion);
     uiShow(uiSuggestions,funcQuestionSuggestion||funcDeepAnalysis);
     msgInfo(`<i>Spørsmålsforslag ${funcQuestionSuggestion?'':'de'}aktivert</i>`);
 }
-window.menuClick_m_Grubling=e=> {
-    funcDeepAnalysis = menuEBold('Grubling', !funcDeepAnalysis);
+window.menuClick_m_grubling=e=> {
+    funcDeepAnalysis = menuEBold('grubling', !funcDeepAnalysis);
     uiShow(uiSuggestions,funcQuestionSuggestion||funcDeepAnalysis);
-    console.log('menuClick_m_Grubling', funcDeepAnalysis)
+    console.log('menuClick_m_grubling', funcDeepAnalysis)
     msgInfo(`<i>Grubling ${funcDeepAnalysis?'':'de'}aktivert</i>`);
 }
-window.menuClick_m_Kontakt=e=>menuClick_OpenUrl('https://www.aigap.no/snakk-med-oss');
-window.menuClick_m_Personvernerklring=e=>menuClick_OpenUrl('https://www.aigap.no/personvernerkl%C3%A6ring');
-window.menuClick_m_Barkode=e=>menuClick_OpenUrl('barcode.jpg');
-window.menuClick_m_Prompt=e=>menuClick_OpenUrl('https://docs.google.com/spreadsheets/d/1mfX64WtObCh7Szyv0zXOscJl0F-_pE3fG0b8rDSSy_c/edit?gid=1531346265#gid=1531346265&range=E4');
-window.menuClick_m_Ungdom=e=>menuShow(false)^menuEBoldOnly('Ungdom', ['Voksen', ...menuClick_alleSpraak])^msgRedoLast('Gjenta siste melding i en språkdrakt som passer for ungdom. Fra nå av skal du svare med ord og på en måte som passer norsk ungdom. Svar med maks femten ord fra nå av med mindre spørsmålet har flere enn femten ord, da skal du bruke like mange ord som i spørsmålet.');
-window.menuClick_m_Voksen=e=>menuShow(false)^menuEBoldOnly('Voksen', ['Ungdom', ...menuClick_alleSpraak])^msgRedoLast('Gjenta siste melding i en språkdrakt som passer for voksne. Fra nå av skal du svare med ord og på en måte som passer voksne. Du trenger ikke svare med maks femten ord lengre.');
-window.menuClick_m_Bokml=e=>menuShow(false)^menuEBoldOnly('Bokml', menuClick_alleSpraak)^msgRedoLast('Gjenta siste melding på bokmål og kortere. Fra nå av skal du kun svare kortfattet på bokmål');
-window.menuClick_m_Nynorsk=e=>menuShow(false)^menuEBoldOnly('Nynorsk', menuClick_alleSpraak)^msgRedoLast('Gjenta siste melding på nynorsk og kortere. Fra nå av skal du kun svare kortfattet på nynorsk');
-window.menuClick_m_Svenska=e=>menuShow(false)^menuEBoldOnly('Svenska', menuClick_alleSpraak)^msgRedoLast('Upprepa senaste meddelandet på svenska och kortare. Från och med nu ska du endast svara kortfattat på svenska.');
-window.menuClick_m_Dansk=e=>menuShow(false)^menuEBoldOnly('Dansk', menuClick_alleSpraak)^msgRedoLast('Gentag sidste besked på dansk og kortere. Fra nu af skal du kun svare kortfattet på dansk.');
-window.menuClick_m_English=e=> menuShow(false)^menuEBoldOnly('English', menuClick_alleSpraak)^msgRedoLast('Repeat last message in English. From now on only answer briefly in English');
-window.menuClick_m_Simuler=e=>{
+window.menuClick_m_kontakt=e=>menuClick_OpenUrl('https://www.aigap.no/snakk-med-oss');
+window.menuClick_m_personvernerklring=e=>menuClick_OpenUrl('https://www.aigap.no/personvernerkl%C3%A6ring');
+window.menuClick_m_barkode=e=>menuClick_OpenUrl('barcode.jpg');
+window.menuClick_m_prompt=e=>menuClick_OpenUrl('https://docs.google.com/spreadsheets/d/1mfX64WtObCh7Szyv0zXOscJl0F-_pE3fG0b8rDSSy_c/edit?gid=1531346265#gid=1531346265&range=E4');
+window.menuClick_m_ungdom=e=>menuShow(false)^menuEBoldOnly('Ungdom', ['Voksen', ...menuClick_alleSpraak])^msgRedoLast('Gjenta siste melding i en språkdrakt som passer for ungdom. Fra nå av skal du svare med ord og på en måte som passer norsk ungdom. Svar med maks femten ord fra nå av med mindre spørsmålet har flere enn femten ord, da skal du bruke like mange ord som i spørsmålet.');
+window.menuClick_m_voksen=e=>menuShow(false)^menuEBoldOnly('Voksen', ['Ungdom', ...menuClick_alleSpraak])^msgRedoLast('Gjenta siste melding i en språkdrakt som passer for voksne. Fra nå av skal du svare med ord og på en måte som passer voksne. Du trenger ikke svare med maks femten ord lengre.');
+window.menuClick_m_bokml=e=>menuShow(false)^menuEBoldOnly('Bokml', menuClick_alleSpraak)^msgRedoLast('Gjenta siste melding på bokmål og kortere. Fra nå av skal du kun svare kortfattet på bokmål');
+window.menuClick_m_nynorsk=e=>menuShow(false)^menuEBoldOnly('Nynorsk', menuClick_alleSpraak)^msgRedoLast('Gjenta siste melding på nynorsk og kortere. Fra nå av skal du kun svare kortfattet på nynorsk');
+window.menuClick_m_svenska=e=>menuShow(false)^menuEBoldOnly('Svenska', menuClick_alleSpraak)^msgRedoLast('Upprepa senaste meddelandet på svenska och kortare. Från och med nu ska du endast svara kortfattat på svenska.');
+window.menuClick_m_dansk=e=>menuShow(false)^menuEBoldOnly('Dansk', menuClick_alleSpraak)^msgRedoLast('Gentag sidste besked på dansk og kortere. Fra nu af skal du kun svare kortfattet på dansk.');
+window.menuClick_m_english=e=> menuShow(false)^menuEBoldOnly('English', menuClick_alleSpraak)^msgRedoLast('Repeat last message in English. From now on only answer briefly in English');
+window.menuClick_m_simuler=e=>{
     uiInput.value = 'Hvordan kommer jeg meg dit?';
     setTimeout(() => { msgSend('Simulate: Hvordan kommer jeg meg dit?|Simulate: Du kan reise til CatoSenteret på Ullevål sykehus med bil, offentlig transport eller tilrettelagte transporttjenester', ()=> { uiInput.value = 'Hva er relevansen til Ullevål sykehus?'; setTimeout(() => { msgSend('Hva er relevansen til Ullevål sykehus?');}, 2000); });}, 2000);
     menuShow(false);
 }
+window.menuClick_m_listmodeller=e=>menuClick_Models(e);
+/*
+const https = require('follow-redirects').https;
+const fs = require('fs');
+
+let options = {
+  'method': 'GET',
+  'hostname': 'api.deepseek.com',
+  'path': '/models',
+  'headers': {
+    'Accept': 'application/json',
+    'Authorization': 'Bearer <TOKEN>'
+  },
+  'maxRedirects': 20
+};
+
+const req = https.request(options, (res) => {
+  let chunks = [];
+
+  res.on("data", (chunk) => {
+    chunks.push(chunk);
+  });
+
+  res.on("end", (chunk) => {
+    let body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+
+  res.on("error", (error) => {
+    console.error(error);
+  });
+});
+
+req.end(); */
+
 window.menuClick_m_Debug=e=>{};
 /////////////// menuClick_m_ - Menu generic ///////////////
 window.menuClickLeaf=e=>{ // handle click on leaf menu item
-    const mi = e.target, mt = mi.innerText.trim(), fn='menuClick_'+menuId(mt)
+    const mi = e.target, mt = mi.innerText.trim(), fn=menuFn(mt)
         , mtp=mi.parentElement.previousElementSibling.innerText.split('\n')[0].trim()
-        , fnp='menuClick_'+menuId(mtp);
+        , fnp=menuFn(mtp);
     if (typeof window[fn] === 'function') window[fn](e);
     else if (typeof window[fnp] === 'function') window[fnp](e, mt);
     else e.target.outerHTML = `window.${fn}=e=> || ${fnp}=(e,sm='${mt}'){};`;
@@ -220,7 +298,7 @@ window.msgSend=(msgQ, onDone)=> {
     let msgQUse = msgQ?.trim() || uiInput.value.trim();
     let r=null;
     if (!msgQUse) msgRedoLast()
-    else if (typeof window['menuClick_'+menuId(msgQUse)] === 'function') window['menuClick_'+menuId(msgQUse)](null);
+    else if (typeof window[menuFn(msgQUse)] === 'function') window[menuFn(msgQUse)](null);
     else {
         if (!msgQ) uiInput.value = '';
         r = msgAsk(msgQUse.split(/\|/)[0]);
@@ -269,11 +347,12 @@ window.msgRecieveTalkAndSend=(t, bIsRetry=false)=> {
 /////////////// AI ///////////////
 const aiRaw2Htm=raw=>{ return raw.replace(/\*\*\*(.*?)\*\*\*/g, '<h2>$1</h2>').replace(/\*\*(.*?)\*\*/g, '<h3>$1</h3>').replace(/#### (.*)/g, '<h4>$1</h4>').replace(/### (.*)/g, '<h3>$1</h3>').replace(/## (.*)/g, '<h2>$1</h2>').replace(/# (.*)/g, '<h1>$1</h1>').replace(/\n/g, '<br/>');}
 , ai2Prompt = a => a.reduce((r, ai, i) => (!i ? [ai] : [...r, { role: "user", content: ai[0] }, { role: "assistant", content: ai[1] }]), [])
-, aiGunn=()=> [...aiGunnar].map((c,i)=>String.fromCharCode((c.charCodeAt()^'gunnar'.charCodeAt(i%6))+32)).join('')
+, aiGun=g=> [...g].map((c,i)=>String.fromCharCode((c.charCodeAt()^'gunnar'.charCodeAt(i%6))+32)).join('')
+, aiGunn=()=>aiGun(aiGunnar) //[...aiGunnar].map((c,i)=>String.fromCharCode((c.charCodeAt()^'gunnar'.charCodeAt(i%6))+32)).join('')
 , aiConfigPipeReplace = 'pipereplace'
 , aiConfigAllModels = [...new Set(aiConfig.flatMap(cfg => (cfg[5] || []).map(m => m[0].split('§')[0])))]
 
-let aiReply=[''], aiHistory = [], aiRequestActiveCount = 0, aiUrl, aiModel, aiGunnar
+let aiReply=[''], aiHistory = [], aiRequestActiveCount = 0, aiUrl='', aiModel='', aiGunnar=''
 
 window.aiReset=()=> {
     aiReply=[''];
@@ -334,10 +413,10 @@ window.aiParseWaitReqBefore=(n = aiConfigTimeout*10)=> {// Wait until aiRequestA
     });
 }
 async function aiParsePerform(f, i) {
-    if (i < f.length) {
-        let m = decodeURIComponent(f[i].trim());
+    if (i < f.length) {// console.log(f)
+        let m = decodeURIComponent(f[i].trim()), fn=menuFn(m)
         if (m.length) {
-            if (typeof window['menuClick_' + menuId(m)] === 'function') await new Promise((resolve) => { window['menuClick_' + menuId(m)](); resolve(); });
+            if (typeof window[fn] === 'function') await new Promise((resolve) => { window[menuFn(m)](); resolve(); });
             else await msgSend(m);
             try { await aiParseWaitReqBefore();}catch(e){}
         }
@@ -353,6 +432,7 @@ menuReset();
 document.addEventListener('click', e => { if (!document.getElementById('menu').contains(e.target) && !document.getElementById('header').contains(e.target)) menuShow(false); });
 uiInput.addEventListener('keydown', e => { if (e.key === 'Enter') msgSend(); });
 aiReset();
-aiParse(aiConfigDefault);uiChat.innerHTML='';
+aiParse(aiConfigDefault);
+uiChat.innerHTML='';
 msgReset();
 aiParse(window.location.search);
--- aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/readme.txt
+++ aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/readme.txt
@@ -1,3 +1,13 @@
 Generert fra chat-kjerne.
 
-Bedre enn chatgpt pga 
+Bedre enn chatgpt pga data privat
+Velg hosting mellom EU, USA og Kina
+
+Velg app
+Fix excel-ark
+Fix conf.js
+app meny og undermenyer og underundermenyer
+Les celle fra worksheet for å få data inn i aiPrompt
+les full config fra celle
+skriv tilbake
+host selv
