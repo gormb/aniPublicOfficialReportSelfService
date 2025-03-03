a8cfe332ac2577f45a294783cf5d36433a2c644b
Gorm Braarvig <gorm_b@hotmail.com>
2025-03-03 15:37:49 +0100

    meny

--- aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/i_setting.js
+++ aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/i_setting.js
@@ -4,19 +4,15 @@ const setting={
     debug:false, dMsg:(k,v)=>{if(setting.debug) {if(v)console.warn(k,v); else console.warn(k)}}
     , menu: `App >>§- ${ cfg.menusForAppProvider('') }
         |Språkdrakt >>§-||Sjargong >>|||Ungdomsspråk|||Voksenspråk§*
-            ||Språk|||Bokmål§*|||Nynorsk|||Sámegiella|||Svenska|||Dansk|||English (UK)|||English (US)        
+            ||Språk >>§-|||Bokmål§*|||Nynorsk|||Sámegiella|||Svenska|||Dansk|||English (UK)|||English (US)        
         |Handling >>§-||Begynn på nytt...||Tøm lagring...
             ||Analyser Personvern||Forsøk alle AI
             ||Utvikling >>§-|||Prompt|||Simuler|||List modeller|||Debug
         |Innstillinger >>§-
-            ||AI >>§-
-                |||AI tilbyder >>§-${ cfg.menusForAiProvider('')}
-                |||Personvernkontroll AI >>§-${cfg.menusForAiProvider('PV ') }
-                |||Bakgrunnsjobb AI >>§-${cfg.menusForAiProvider('BG ') }
-                |||Tilfeldige AI-tilbydere
-            ||Sikkerhet >>§-
-                |||Sensitive data >>§-||||Ikke send sensitive data||||Omformuler sensitive data||||Godta sensitive data§*
+            ||Sikkerhet >>§-|||Sensitive data >>§-||||Ikke send sensitive data||||Omformuler sensitive data||||Godta sensitive data§*
                 |||Helseråd fra AI >>§-||||Ikke mottatt helseråd fra AI||||Omformuler helseråd||||Godta helseråd§*
+            ||AI >>§-|||AI tilbyder >>§-${ cfg.menusForAiProvider('')}|||Personvernkontroll AI >>§-${cfg.menusForAiProvider('PV ') }|||Bakgrunnsjobb AI >>§-${cfg.menusForAiProvider('BG ') }
+                |||Tilfeldige AI-tilbydere
             ||Analyse >>§-|||Spørsmålsforslag§ *
                 |||Grubling
             ||Lagre lokalt
