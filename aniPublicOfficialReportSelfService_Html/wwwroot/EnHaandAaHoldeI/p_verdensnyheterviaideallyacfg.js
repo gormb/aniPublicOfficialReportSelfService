5181130c7cf05c82cc35d2e03fa1a2e8f6fc4889
Gorm Braarvig <gorm_b@hotmail.com>
2025-03-14 10:47:19 +0100

    qwer

--- aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/p_verdensnyheterviaideallyacfg.js
+++ aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/p_verdensnyheterviaideallyacfg.js
@@ -1,7 +1,7 @@
 // Felles config; skjul menyer set visuell profil
-msgSend('groklatest')^cfg.visAppMeny(false)^ui.c.ImgAVugg(3,1);
+msgSend('gpt4search')^cfg.visAppMeny(false)^ui.c.ImgAVugg(3,1);
 ui.c.ImgA = 'https://news.ideallya.com/icons/Icon-maskable-512.png';
-ui.c.ImgQrUrl = 'p_verdensnyheterviaideallya.png'
+ui.c.ImgQrUrl = 'p_verdensnyheterviaideallyaqr.png'
 
 document.documentElement.style.setProperty('--primary-color', '#00f');
 document.documentElement.style.setProperty('--font-family', "'Roboto', sans-serif");
