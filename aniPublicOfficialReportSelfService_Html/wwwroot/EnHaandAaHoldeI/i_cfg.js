ec8118482e7ef443c2d550f05009b213cfa4c75c
Gorm Braarvig <gorm_b@hotmail.com>
2025-03-23 22:17:46 +0100

    qwe

--- aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/i_cfg.js
+++ aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/i_cfg.js
@@ -21,10 +21,10 @@ const cfg={
             ,'Assistert personlig støtte >>§-', ['NO Din Offentlige Partner','NO Min Digitale Venn','NO RettighetsVakten','NO KlarTale','NO HverdagsHjelpen']
             ,'Nyheter >>§-',['Verdens nyheter via Ideallya']]
         ]]
-    , appProvider:cfg.appProvider_Stat
-    , appProvider_Db:[[]]
-    , appProvider1:()=>Object.entries([...cfg.appProvider_Stat.flatMap(([m,s])=>s.flatMap((v,i,a)=>i%2?v.map(App=>({App,mor:a[i-1].slice(0,-5),mormor:m.slice(0,-5)})):[]),...Object.values(cfg.appProvider_Db.reduce((a,r)=>(a[r.App]=r,a),{})))].reduce((o,{App,mor,mormor})=>((o[mormor+' >>§-']??={})[mor+' >>§-']??=new Set()).add(App),o={})&&o).map(([m,s])=>[m,Object.entries(s).flatMap(([k,v])=>[k,[...v]])])
-
+    , appProvider3:this.appProvider_Stat
+    , appProvider_Db:[['Generelt','Ny','Koblingsfeil!']]
+    , appProvider1:()=>cfg.appProvider_Stat//
+        //Object.entries([...cfg.appProvider_Stat.flatMap(([m,s])=>s.flatMap((v,i,a)=>i%2?v.map(App=>({App,mor:a[i-1].slice(0,-5),mormor:m.slice(0,-5)})):[]),...Object.values(cfg.appProvider_Db.reduce((a,r)=>(a[r.App]=r,a),{})))].reduce((o,{App,mor,mormor})=>((o[mormor+' >>§-']??={})[mor+' >>§-']??=new Set()).add(App),o={})&&o).map(([m,s])=>[m,Object.entries(s).flatMap(([k,v])=>[k,[...v]])])
     , appProvider2: () => {
         const out = {};
         [...cfg.appProvider_Stat.flatMap(([m, s]) =>
@@ -37,9 +37,9 @@ const cfg={
         });
         return Object.entries(out).map(([m, s]) => [m, Object.entries(s).flatMap(([k, v]) => [k, [...v]])]);
     }
-    , menusForAppProvider: () => cfg.appProvider.map(([pt, subs]) => `||${pt}` + subs.reduce((acc, cur, i, a) => i % 2 === 0 ? acc + `|||${cur}` + (Array.isArray(a[i+1]) ? a[i+1].map(x => `||||${x}`).join('') : '') : acc, '')).join('')
+    , menusForAppProvider: () => cfg.appProvider1().map(([pt, subs]) => `||${pt}` + subs.reduce((acc, cur, i, a) => i % 2 === 0 ? acc + `|||${cur}` + (Array.isArray(a[i+1]) ? a[i+1].map(x => `||||${x}`).join('') : '') : acc, '')).join('')
     , visAppMeny:b=>ui.Show(mc0,b)^ui.Show(mc0.previousElementSibling,b)^ui.Show(mc0.nextElementSibling,b)
-    , appList:()=>cfg.appProvider.flatMap(([_, subs])=>subs.flatMap((s,i,a)=>i%2==0&&Array.isArray(a[i+1])?a[i+1]:[]).filter(Boolean))
+    , appList:()=>cfg.appProvider1().flatMap(([_, subs])=>subs.flatMap((s,i,a)=>i%2==0&&Array.isArray(a[i+1])?a[i+1]:[]).filter(Boolean))
     , aiPromptWelcomeQuestion:`Hva er velkomstmeldingen?`
     , aiPromptWelcome:`Velkommen til chat.<br/><br/><i>Vi prioriterer personvern. Spørsmål lagres ikke, data sendes til en språkmodell. Mer om personvern under Sikkerhet >> Personvern.</i><br/><br/>Hva lurer du på?`
     , aiPrompt:[{ role: `system`, content: 
