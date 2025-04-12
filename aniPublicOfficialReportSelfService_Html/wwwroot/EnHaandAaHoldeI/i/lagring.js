6a6239eaefa58b9374f47d3f4be2e879055ce909
Gorm Braarvig <gorm_b@hotmail.com>
2025-04-12 20:25:40 +0200

    qwe

--- aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/i/lagring.js
+++ aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/i/lagring.js
@@ -16,7 +16,7 @@ const lagring = {
     , sjargong: 'voksensprk'
     , spraak: 'bokml'
     , qr:()=>window.location.origin+window.location.pathname
-        +'?'+lagring.getAktivApp()+'?'+lagring.getAi(0,'')
+        +'?'+lagring.getAktivApp()+/*'?'+lagring.getAi(0,'')*/
         +(lagring.spraak=='bokml'?'':'?'+lagring.spraak)
         +(lagring.sjargong=='voksensprk'?'':'?'+lagring.sjargong)
     , delay:100, delayI:100
