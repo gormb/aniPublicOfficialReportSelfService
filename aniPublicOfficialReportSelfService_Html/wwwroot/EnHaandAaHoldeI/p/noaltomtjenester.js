11b7c95240561236f2aae1fe8b93aa8909b70e5f
Gorm Braarvig <gorm_b@hotmail.com>
2025-04-30 14:20:28 +0200

    qwe

--- aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/p/noaltomtjenester.js
+++ aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/p/noaltomtjenester.js
@@ -1,13 +1,275 @@
-cfg_aiPromptWelcome = `Alt Om Tjenester 📚
+cfg_aiPromptWelcome = `📚 Alt Om Tjenester
 
-Få full oversikt over offentlige rettigheter, plikter og tjenester – pluss praktiske råd for å ta dem i bruk.
+[detaljer c='Assistenten hjelper deg med...']
+<b>Jeg vil hjelpe deg</b>
+Jeg gir deg oversikt over hva det offentlige tilbyr – og hva du som innbygger har av rettigheter og plikter.
+Dette gjelder alt fra helse og skole til trygd, klage og skatt.
+[/detaljer]
+
+[detaljer c='<b>Tjenesteområder...</b>']
+<b>Tjenesteområder</b>
+[detaljer c='1. Helse og omsorg...']
+<b>1. Helse og omsorg</b>
+[detaljer c='1a. Fastlege, helsehjelp, frikort...']
+<b>1a. Fastlege, helsehjelp, frikort</b>
+  1a1. Fastlege
+  1a2. Rett til helsehjelp
+  1a3. Frikort
+  1a4. Egenandel
+  1a5. Bytte lege
+  1a6. Ventetid
+  1a7. Klage på helsehjelp
+  [/detaljer]
+  [detaljer c='1b. Tannhelse og hjelpemidler...']
+  <b>1b. Tannhelse og hjelpemidler</b>
+  1b1. Gratis tannlege barn
+  1b2. HELFO-refusjon
+  1b3. Tilskudd voksne
+  1b4. Hjelpemiddelsentralen
+  1b5. Ortopediske hjelpemidler
+  1b6. Reisetilskudd
+  1b7. Klage på vedtak
+  [/detaljer]
+[/detaljer]
+[detaljer c='2. Familie og barn...']
+<b>2. Familie og barn</b>
+[detaljer c='2a. Barnetrygd og støtteordninger...']
+<b>2a. Barnetrygd og støtteordninger</b>
+2a1. Barnetrygd
+2a2. Ekstra barnetrygd
+2a3. Utvidet barnetrygd
+2a4. Kontantstøtte
+2a5. Engangsstønad
+2a6. Barnebidrag
+2a7. Søknad og dokumentasjon
+[/detaljer]
+[detaljer c='2b. Barnehage og skole...']
+<b>2b. Barnehage og skole</b>
+2b1. Barnehageplass
+2b2. Redusert foreldrebetaling
+2b3. Spesialpedagogisk hjelp
+2b4. Skoleplass og innskriving
+2b5. Skoleskyss
+2b6. Helsesykepleier
+2b7. Gratis skolemat
+[/detaljer]
+[/detaljer]
+[detaljer c='3. Arbeid og sosiale rettigheter...']
+<b>3. Arbeid og sosiale rettigheter</b>
+[detaljer c='3a. Arbeidsledighet og sykepenger...']
+<b>3a. Arbeidsledighet og sykepenger</b>
+3a1. Registrering hos NAV
+3a2. Dagpenger
+3a3. Sykepenger
+3a4. Arbeidsavklaringspenger (AAP)
+3a5. Legeerklæring og meldekort
+3a6. Oppfølging og aktivitetsplan
+3a7. Klage på vedtak
+[/detaljer]
+[detaljer c='3b. Uføretrygd og pensjon...']
+<b>3b. Uføretrygd og pensjon</b>
+3b1. Søknad om uføretrygd
+3b2. Ung ufør
+3b3. Alderspensjon
+3b4. AFP (avtalefestet pensjon)
+3b5. Minstepensjon
+3b6. Kombinere arbeid og pensjon
+3b7. Etterlatte og barnepensjon
+[/detaljer]
+[detaljer c='3c. Sosialhjelp og økonomisk støtte...']
+<b>3c. Sosialhjelp og økonomisk støtte</b>
+3c1. Økonomisk sosialhjelp
+3c2. Nødhjelp
+3c3. Strømstøtte
+3c4. Bostøtte
+3c5. Økonomisk rådgivning i kommunen
+3c6. Gjeldsrådgivning
+3c7. Digital søknad og dokumentasjon
+[/detaljer]
+[/detaljer]
+[detaljer c='4. Utdanning og kompetanse...']
+<b>4. Utdanning og kompetanse</b>
+[detaljer c='4a. Støtte og rettigheter for elever og studenter...']
+<b>4a. Støtte og rettigheter for elever og studenter</b>
+4a1. Stipend og lån fra Lånekassen
+4a2. Utstyrsstipend
+4a3. Borteboerstipend
+4a4. Tilleggslån og sykdom
+4a5. Rettigheter ved sykdom og permisjon
+4a6. Søknadsfrister
+4a7. Klage og dokumentasjon
+[/detaljer]
+[detaljer c='4b. Videregående, høyere utdanning og voksenopplæring...']
+<b>4b. Videregående, høyere utdanning og voksenopplæring</b>
+4b1. Opptak til videregående skole
+4b2. Universitets- og høyskoleopptak
+4b3. Voksenrett og voksenopplæring
+4b4. Realkompetansevurdering
+4b5. Gratis opplæring for voksne
+4b6. Karrieresenter og rådgivning
+4b7. Fullføring og rettigheter
+[/detaljer]
+[detaljer c='4c. Kompetanse og arbeid...']
+<b>4c. Kompetanse og arbeid</b>
+4c1. Kompetansepluss
+4c2. Utdanning gjennom NAV
+4c3. Lærlingordning
+4c4. Etter- og videreutdanning
+4c5. Kurs i digital kompetanse
+4c6. Godkjenning av utenlandsk utdanning
+4c7. Midlertidige kvalifiseringstiltak
+[/detaljer]
+[/detaljer]
+[detaljer c='5. Skatt og økonomi...']
+<b>5. Skatt og økonomi</b>
+[detaljer c='5a. Personskatt og skattemelding...']
+<b>5a. Personskatt og skattemelding</b>  
+5a1. Skattemelding (selvangivelse)  
+5a2. Fradrag og pendlerfradrag  
+5a3. Restskatt og tilbakebetaling  
+5a4. Skatteoppgjør og utbetaling  
+5a5. Endre eller klage på skattemelding  
+5a6. Digital innlevering og Altinn  
+5a7. Hjelp fra Skatteetaten
+[/detaljer]
+[detaljer c='5b. Avgifter og eiendom...']
+<b>5b. Avgifter og eiendom</b>  
+5b1. Merverdiavgift (MVA)  
+5b2. Eiendomsskatt  
+5b3. Dokumentavgift  
+5b4. Bompenger og veibruksavgift  
+5b5. Avgift på kjøretøy  
+5b6. Arv og gaveskatt  
+5b7. Kommunale avgifter
+[/detaljer]
+[detaljer c='5c. Offentlige betalinger og gjeld...']
+<b>5c. Offentlige betalinger og gjeld</b>  
+5c1. eFaktura og Digipost  
+5c2. Ubetalte regninger til det offentlige  
+5c3. Innkreving fra Statens innkrevingssentral  
+5c4. Gebyrer og betalingsutsettelse  
+5c5. Skattegjeld og avdragsordninger  
+5c6. Offentlige inkassotiltak  
+5c7. Rett til å be om nedbetaling
+[/detaljer]
+[/detaljer]
+[detaljer c='6. Innbyggerdialog og dokumentasjon...']
+<b>6. Innbyggerdialog og dokumentasjon</b>
+[detaljer c='6a. Folkeregister og ID...']
+<b>6a. Folkeregister og ID</b>  
+6a1. Endre folkeregistrert adresse  
+6a2. Registrere flytting til og fra utlandet  
+6a3. Fødselsnummer og D-nummer  
+6a4. Fødsel og navnevalg  
+6a5. Vergemål og fullmakt  
+6a6. ID-kort og legitimasjon  
+6a7. Klage og retting av feil
+[/detaljer]
+[detaljer c='6b. Klage og innsyn...']
+<b>6b. Klage og innsyn</b>  
+6b1. Klage på vedtak fra offentlig etat  
+6b2. Klagefrist og hvordan skrive klage  
+6b3. Innsyn i offentlige dokumenter  
+6b4. Rett til begrunnelse for vedtak  
+6b5. Innsyn i egne personopplysninger  
+6b6. Taushetsplikt og unntak  
+6b7. Svarfrister og purring
+[/detaljer]
+[detaljer c='6c. Digital kommunikasjon...']
+<b>6c. Digital kommunikasjon</b>  
+6c1. Altinn og digital postkasse  
+6c2. ID-porten og BankID  
+6c3. Tilgang til offentlige tjenester på nett  
+6c4. Fullmakter og representasjon  
+6c5. Digitale kvitteringer og varslinger  
+6c6. Sikker innlogging og personvern  
+6c7. Hjelp ved tekniske problemer
+[/detaljer]
+[/detaljer]
+[detaljer c='7. Næringsliv og gründere...']
+<b>7. Næringsliv og gründere</b>
+[detaljer c='7a. Starte og drive bedrift...']
+<b>7a. Starte og drive bedrift</b>
+7a1. Registrere foretak
+7a2. Enhetsregisteret
+7a3. Starte AS
+7a4. ENK eller AS?
+7a5. Brønnøysundregistrene
+7a6. HMS-kort og ID
+7a7. Klage og veiledning
+[/detaljer]
+[detaljer c='7b. Tilskudd og støtte...']
+<b>7b. Tilskudd og støtte</b>
+7b1. Innovasjon Norge
+7b2. Landbrukstilskudd
+7b3. Tilskudd ved omstilling
+7b4. Midler for unge gründere
+7b5. Energistøtte
+7b6. SkatteFUNN
+7b7. Søknad og rapportering
+[/detaljer]
+[/detaljer]
+[detaljer c='8. Mobilitet og reise...']
+<b>8. Mobilitet og reise</b>
+[detaljer c='8a. Kjøretøy og førerkort...']
+<b>8a. Kjøretøy og førerkort</b>  
+8a1. Fornye førerkort  
+8a2. Melde tapt eller stjålet førerkort  
+8a3. Registrere kjøretøy  
+8a4. Omregistrering ved kjøp/salg  
+8a5. EU-kontroll og kjøretøykontroll  
+8a6. Trafikkforsikring og ansvar  
+8a7. Klage på gebyr eller vedtak
+[/detaljer]
+[detaljer c='8b. Kollektivtransport og tilrettelegging...']
+<b>8b. Kollektivtransport og tilrettelegging</b>  
+8b1. TT-kort (transporttjeneste for funksjonshemmede)  
+8b2. Honnørkort og ledsagerbevis  
+8b3. Skoleskyss og fritidsskyss  
+8b4. Tilrettelagt transport ved sykdom  
+8b5. Reiserefusjon fra HELFO  
+8b6. Klage på rute, tilbud eller tilgjengelighet  
+8b7. Finn transportordninger i kommunen
+[/detaljer]
+[detaljer c='8c. Reise og opphold i utlandet...']
+<b>8c. Reise og opphold i utlandet</b>  
+8c1. Reise med barn – samtykke og dokumentasjon  
+8c2. Pass og nødpass  
+8c3. Reise med medisin og hjelpemidler  
+8c4. Rett til helsehjelp i EU/EØS (EHIC/HELFO)  
+8c5. Innreise og opphold i Norge  
+8c6. Varsling og reiseregistrering (UD)  
+8c7. Utenriksstasjoner og konsulær hjelp
+[/detaljer]
+[/detaljer]
+[/detaljer]
+
+[detaljer c='Tiltenkte brukere...']
+<b>Denne løsningen er tiltenkt</b>
+- Deg som vil forstå hvilke rettigheter du har
+- Deg som vil vite hva det offentlige forventer av deg
+- Deg som vil ha hjelp til å finne riktig etat eller lovhjemmel
+- Deg som vil hjelpe andre (som pårørende, lærer eller rådgiver)
+[/detaljer]
 
 Hva vil du vite mer om?`;
 
 cfg.aiPrompt= [
   { role: `system`, content:
-    `Du er en digital assistent som gir en helhetlig oversikt over offentlige tjenester. Du forklarer hvilke rettigheter og plikter du har, og gir praktiske råd om hvordan du får tilgang til tjenestene.  
-Still gjerne oppfølgingsspørsmål for å tilpasse veiledningen til brukerens situasjon.`
+    `Du er ikke bare en hjelpsom chatbot – du er en offentlig veiviser.
+
+Du hjelper brukeren med å forstå hvordan det offentlige fungerer: hvilke rettigheter og plikter man har som innbygger, og hvilke tjenester staten, NAV, kommunen, helsevesenet, skatteetaten og utdanningssystemet tilbyr.
+
+Du forklarer på en enkel, nøktern og ryddig måte – som en veileder i informasjonsforvaltning. Du bygger oversikt, ikke bare raske svar.
+
+Du hjelper også brukeren å se sammenhengene:
+– Hvorfor finnes denne tjenesten?
+– Hvem gjelder den for?
+– Hvordan søker man, og hva må man gjøre selv?
+
+Du er ikke bare en rådgiver, du er et kompass i møte med offentlig sektor.
+Still gjerne oppfølgingsspørsmål for å tilpasse informasjonen til brukerens livssituasjon.
+Målet er at brukeren skal forstå systemet – og bruke det.`
   },
   [`Hvilke helsetjenester har jeg rett til?`, `Du har rett til gratis helsesjekk og akutt behandling. Er du registrert hos din fastlege?`],
   [`Hvordan søker jeg om sosialhjelp?`, `Sosialhjelp søkes via din kommune. Har du fått veiledning fra kommunen tidligere?`],
@@ -16,4 +278,4 @@ Still gjerne oppfølgingsspørsmål for å tilpasse veiledningen til brukerens s
   [`Hva er mine rettigheter som student?`, `Studenter kan få stipend og lån fra Lånekassen. Er du student ved en godkjent institusjon?`]
 ];
 
-cfg.set(cfg_aiPromptWelcome,'(NO) Alt Om Tjenester','mistrallarge','p/noaltomtjenester.png','f,5,2','#fbbf24', null, 'Lexend')
+cfg.set(cfg_aiPromptWelcome,'(NO) Alt Om Tjenester','mistrallarge','p/noaltomtjenester.png','f,5,2','#ab8f04', null, 'Lexend')
