573b63d7297d9c902d5edf127c34e97b22c985e8
Gorm Braarvig <gorm_b@hotmail.com>
2025-05-01 13:56:01 +0200

    qwe

--- aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/p/noaltomkommunaletjenester.js
+++ aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/p/noaltomkommunaletjenester.js
@@ -1,81 +1,159 @@
-cfg_aiPromptWelcome = `📚 Alt Om Kommunale Tjenester
+cfg_aiPromptWelcome = `🏛️ Alt om kommunale tjenester
 
-[detaljer c='Jeg vil hjelpe deg...']
-<b>Jeg vil hjelpe deg</b>
-Jeg gir deg oversikt over hva det offentlige tilbyr – og hva du som innbygger har av rettigheter og plikter.
-Dette gjelder alt fra helse og skole til trygd, klage og skatt.
-[/detaljer]
+[detaljer c='Hva jeg hjelper deg med...']
+Jeg gir deg oversikt over kommunens tjenester, rettigheter og plikter – og hvordan du finner frem i hjelpetilbudene.  
 
-[detaljer c='<b>Tjenesteområder...</b>']
-<hr><b>Tjenesteområder</b>
-[detaljer c='A. Helse og omsorg...']
-<hr><b>A. Helse og omsorg</b>
-[detaljer c='A1. Hjemmebaserte tjenester...']
-<b>A1. Hjemmebaserte tjenester</b>
-[detaljer c='A1a. Hjemmesykepleie...']
-<b>A1a. Hjemmesykepleie</b>  
-- Hjelp til medisinsk behandling i hjemmet  
-- Vanlig for eldre og kronisk syke  
-- Måles i KOSTRA bl.a. ved "Andel brukere med omfattende bistandsbehov" og "Antall timer per uke per bruker"  
-[/detaljer]
-[detaljer c='A1b. Praktisk bistand...']
-<b>A1b. Praktisk bistand</b>  
-- Hjelp til rengjøring, innkjøp og mat  
-- Tildeles etter behovsvurdering  
-- Måles i KOSTRA som "Tjenestemottakere i aldersgruppen 67+ pr. 1000 innbyggere"
-[/detaljer]
-[detaljer c='A1c. Trygghetsalarm og velferdsteknologi...']
-<b>A1c. Trygghetsalarm og velferdsteknologi</b>  
-- Teknologi for økt trygghet hjemme  
-- Alarm, fallregistrering, medisindispensere  
-- KOSTRA: "Andel mottakere av velferdsteknologi"
-[/detaljer]
-[detaljer c='A1d. Brukerstyrt personlig assistanse (BPA)...']
-<b>A1d. Brukerstyrt personlig assistanse (BPA)</b>  
-- For personer med stort behov for praktisk og personlig hjelp  
-- Målet er selvstendighet og frihet  
-- KOSTRA: "Antall brukere og timer BPA"
-[/detaljer]
-[detaljer c='A1e. Avlastning i hjemmet...']
-<b>A1e. Avlastning i hjemmet</b>  
-- Gis til pårørende med særlig tyngende omsorgsansvar  
-- Ofte for foreldre med barn med store hjelpebehov  
-- Måles som antall mottakere og tildelte timer
-[/detaljer]
-[detaljer c='A1f. Omsorgslønn...']
-<b>A1f. Omsorgslønn</b>  
-- Lønn til pårørende som yter omsorg  
-- Vurderes ut fra omfang og tyngde  
-- KOSTRA: "Antall mottakere av omsorgslønn"
-[/detaljer]
-[detaljer c='A1g. Matombringing...']
-<b>A1g. Matombringing</b>  
-- Levering av ferdiglagde måltider til hjemmeboende  
-- KOSTRA: "Antall brukere og enhetspris per porsjon"
-[/detaljer]
-<hr>[/detaljer]
-[detaljer c='B. Oppvekst og utdanning...']<b>B. Oppvekst og utdanning</b>[/detaljer]
-[detaljer c='C. Sosiale tjenester og bolig...']<b>C. Sosiale tjenester og bolig</b>[/detaljer]
-[detaljer c='D. Barnevern...']<b>D. Barnevern</b>[/detaljer]
-[detaljer c='E. Kultur, idrett og frivillighet...']<b>E. Kultur, idrett og frivillighet</b>[/detaljer]
-[detaljer c='F. Næring, miljø og samfunnsutvikling...']<b>F. Næring, miljø og samfunnsutvikling</b>[/detaljer]
-[detaljer c='G. Plan, bygg og eiendomsforvaltning...']<b>G. Plan, bygg og eiendomsforvaltning</b>[/detaljer]
-[detaljer c='H. Teknisk sektor og samferdsel...']<b>H. Teknisk sektor og samferdsel</b>[/detaljer]
-[detaljer c='I. Beredskap og samfunnssikkerhet...']<b>I. Beredskap og samfunnssikkerhet</b>[/detaljer]
-[detaljer c='J. Politisk styring, administrasjon og støttefunksjoner...']<b>J. Politisk styring, administrasjon og støttefunksjoner</b>[/detaljer]
-
-
-
+Du får også vite hvordan kvalitet og ressursbruk måles.
 [/detaljer]
 
+[detaljer c='Kommunale tjenester...']<hr><h2>Kommunale tjenester</h2>
 
-[detaljer c='Tiltenkte brukere...']
-<b>Tiltenkte brukere</b>
-- Deg som vil forstå hvilke rettigheter du har
-- Deg som vil vite hva det offentlige forventer av deg
-- Deg som vil ha hjelp til å finne riktig etat eller lovhjemmel
-- Deg som vil hjelpe andre (som pårørende, lærer eller rådgiver)
+[detaljer c='A. Helse og omsorg...']<b>A. Helse og omsorg</b>
+A1. Hjemmebaserte tjenester – hjemmesykepleie, praktisk bistand, BPA  
+Målt på: andel brukere, timer per uke, ressursbruk per innbygger
+
+A2. Institusjonsbaserte tjenester – sykehjem, korttidsopphold, dagsenter  
+Målt på: plasser per 1000 innbyggere, beleggsprosent, utskrivningstid
+
+A3. Rehabilitering og habilitering – fysio, ergo, logoped  
+Målt på: antall brukere, ventetid, effektmåling
+
+A4. Psykisk helse og rus – lavterskel, oppfølging, behandling  
+Målt på: antall brukere, reinnleggelse, brukertilfredshet
+
+A5. Legetjenester og legevakt – fastlege, legevakt, helsestasjon  
+Målt på: konsultasjoner per innbygger, tilgjengelighet, ventetid
+
+A6. Helsestasjon og skolehelsetjeneste – svangerskap, barn, ungdom  
+Målt på: dekning, andel fullførte kontroller, brukertilfredshet
+
+A7. Tannhelsetjeneste – barn, voksne med behov, forebygging  
+Målt på: andel undersøkt, behandlingsrate, kostnad per bruker
 [/detaljer]
+
+[detaljer c='B. Oppvekst og utdanning...']<hr><b>B. Oppvekst og utdanning</b>
+B1. Barnehage – plass, tilrettelegging, bemanning  
+Målt på: dekning, utgift per barn, bemanningsnorm
+
+B2. Grunnskole – undervisning, SFO, spesialundervisning  
+Målt på: utgift per elev, lærertetthet, resultater
+
+B3. Videregående opplæring – studieforberedende, yrkesfag, lærlinger  
+Målt på: gjennomføring, læreplassandel, karakterer
+
+B4. Voksenopplæring – grunnskole, norsk, samfunnskunnskap  
+Målt på: deltakelse, progresjon, beståttandel
+
+B5. Kulturskole – musikk, dans, kunst  
+Målt på: deltakelse per 1000 barn, kostnad per elev
+
+B6. Bibliotek – utlån, digitale tjenester, arrangementer  
+Målt på: besøk per innbygger, utlån, aktivitetstilbud
+
+B7. Fritid og ungdom – klubber, ferieaktiviteter, idrett  
+Målt på: antall deltakere, åpningstimer, tilskudd til lag
+[/detaljer]
+
+[detaljer c='C. Sosiale tjenester og bolig...']<hr><b>C. Sosiale tjenester og bolig</b>
+C1. Økonomisk sosialhjelp – livsopphold, nødhjelp, rådgivning  
+Målt på: antall mottakere, gjennomsnittlig støtte, saksbehandlingstid
+
+C2. Bolig – kommunale boliger, bostøtte, startlån  
+Målt på: tildelingsrate, saksbehandlingstid, bomiljøtiltak
+
+C3. Introduksjon og kvalifisering – flyktning, arbeidsrettet  
+Målt på: deltagelse, overgang til arbeid/utdanning, gjennomføring
+
+C4. Flyktningtjenester – bosetting, helsehjelp, integrering  
+Målt på: antall bosatte, deltagelse i program, språkprøver
+
+C5. Tilrettelagt bolig – omsorgsbolig, bemannet bolig  
+Målt på: antall boliger, dekningsgrad, tilfredshet
+
+C6. Midlertidig bolig – akuttplasser, nødløsninger  
+Målt på: brukerdager, utskrivningstid, tilbakefall
+
+C7. Oppfølgingstjenester – gjeldsrådgivning, rus, psykisk helse  
+Målt på: antall brukere, gjennomføring, effekt
+[/detaljer]
+
+[detaljer c='D. Barnevern...']<hr><b>D. Barnevern</b>
+D1. Meldinger og undersøkelser – bekymringsmeldinger, utredning  
+Målt på: antall meldinger, andel fullførte undersøkelser
+
+D2. Hjelpetiltak – veiledning, avlastning, økonomisk støtte  
+Målt på: antall tiltak, varighet, effekt
+
+D3. Omsorgstiltak – fosterhjem, institusjon, ettervern  
+Målt på: antall plasseringer, stabilitet, skolegang
+
+D4. Forebygging – foreldrestøtte, samarbeidsmøter  
+Målt på: antall familier, deltakelse, effekt
+
+D5. Akuttberedskap – akuttplassering, barnevernvakt  
+Målt på: antall saker, responstid, varighet
+[/detaljer]
+
+[detaljer c='E. Kultur, idrett og frivillighet...']<hr><b>E. Kultur, idrett og frivillighet</b>
+E1. Kulturtilbud – bibliotek, museum, arrangementer  
+Målt på: besøk, aktivitetstilbud, utgift per innbygger
+
+E2. Idrett – anlegg, svømmehall, aktivitetstilbud  
+Målt på: anlegg per innbygger, bruk, kostnad
+
+E3. Frivillighet – tilskudd, møteplasser, inkludering  
+Målt på: antall lag/støttemottakere, aktivitetstimer
+[/detaljer]
+
+[detaljer c='F. Næring, miljø og samfunnsutvikling...']<hr><b>F. Næring, miljø og samfunnsutvikling</b>
+F1. Næringsutvikling – etablererstøtte, rådgivning  
+Målt på: antall bedrifter støttet, effekt
+
+F2. Landbruk og miljø – tilskudd, vilt, klima  
+Målt på: antall søknader, dekningsgrad, miljøeffekt
+
+F3. Samfunnsutvikling – stedsutvikling, planarbeid  
+Målt på: fullførte planer, innbyggerinvolvering
+[/detaljer]
+
+[detaljer c='G. Plan, bygg og eiendomsforvaltning...']<hr><b>G. Plan, bygg og eiendomsforvaltning</b>
+G1. Byggesak og plan – byggesøknader, dispensasjon  
+Målt på: saksbehandlingstid, antall saker
+
+G2. Eiendom – kommunale bygg, utleie  
+Målt på: vedlikeholdsgrad, utnyttelsesgrad
+
+G3. Kart og oppmåling – matrikkel, deling  
+Målt på: saksbehandlingstid, kvalitet
+[/detaljer]
+
+[detaljer c='H. Teknisk sektor og samferdsel...']<hr><b>H. Teknisk sektor og samferdsel</b>
+H1. Vei og park – brøyting, belysning, grøntområder  
+Målt på: km vei vedlikeholdt, grøntareal per innbygger
+
+H2. Vann og avløp – forsyning, rensing  
+Målt på: leveringsgrad, vannkvalitet
+
+H3. Renovasjon – avfall, gjenvinning  
+Målt på: mengde avfall, sorteringsgrad
+[/detaljer]
+
+[detaljer c='I. Beredskap og samfunnssikkerhet...']<hr><b>I. Beredskap og samfunnssikkerhet</b>
+I1. Beredskap – planverk, øvelser  
+Målt på: oppdateringsgrad, øvingsfrekvens
+
+I2. Brann og redning – forebygging, utrykning  
+Målt på: responstid, antall oppdrag
+[/detaljer]
+
+[detaljer c='J. Politisk styring og administrasjon...']<hr><b>J. Politisk styring og administrasjon</b>
+J1. Politisk ledelse – kommunestyre, utvalg  
+Målt på: møteaktivitet, saker behandlet
+
+J2. Administrasjon – økonomi, personal, IKT  
+Målt på: kostnad per innbygger, effektivitet
+[/detaljer]
+<br><hr>[/detaljer]
 
 Hva vil du vite mer om?`;
 
