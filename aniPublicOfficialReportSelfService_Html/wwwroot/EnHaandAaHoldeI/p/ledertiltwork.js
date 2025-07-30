a6b48d627e7286a70344249cbdf113ac8ac252e0
Gorm Braarvig <gorm_b@hotmail.com>
2025-07-30 11:20:32 +0200

    qwe

--- aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/p/ledertiltwork.js
+++ aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/p/ledertiltwork.js
@@ -1,10 +1,32 @@
 cfg_aiPromptWelcome = `Velkommen.
 
-[detaljer c='Tilt.work...']Tilt.work er etablert for å møte et voksende behov i markedet for helhetlig, senior rådgivning til virksomheter i endring. Navnet reflekterer vår filosofi: små, strategiske justeringer kan gi betydelige positive effekter over tid.[/detaljer]
+[detaljer c='tilt.work...']tilt.work er et høykompetent team av konsulenter med komplementær kompetanse innen ledelse, finans, HR, drift, kommersiell utvikling og teknologi. Vår oppgave er bistå små og mellomstore virksomheter gjennom kritiske utviklingsfaser og endringsprosesser.[/detaljer]
 
 [detaljer c='McGyver-team...']Vi fokuserer på norske virksomheter med vekstambisjoner, inkludert selskaper som ekspanderer internasjonalt. Vår tilnærming er å fungere som et «McGyver-team» – erfarne problemløsere som kan håndtere komplekse utfordringer med kreativitet og presisjon.[/detaljer]
 
-[detaljer c='McGyver-team...']tilt.work er et høykompetent team av konsulenter med komplementær kompetanse innen ledelse, finans, HR, drift, kommersiell utvikling og teknologi. Vår oppgave er bistå små og mellomstore virksomheter gjennom kritiske utviklingsfaser og endringsprosesser.[/detaljer]
+[detaljer c='Partnere...']<img src="https://tilt.work/wp-content/uploads/2025/04/paal_sq-150x150.jpg"><img>
+<b>Paal Leveraas</b> (paal.leveraas@tilt.work) 
+daglig leder med over 40 års leder og coachingerfaring.
+
+<img src="https://tilt.work/wp-content/uploads/2025/04/Thomas_sq-150x150.jpg"><img>
+<b>Thomas Nygaard</b> (thomas.nygaard@tilt.work) 
+styreleder med solid ledererfaring fra store virksomheter.
+
+<img src="https://tilt.work/wp-content/uploads/2025/04/hans-jacob-480-sq-uformell-150x150.jpg"><img>
+<b>Hans Jacob Christensen</b> (hans.jacob.christensen@tilt.work) 
+partner og HR-veteran med dyp innsikt i det menneskelige og strategisk rådgivning.
+[/detaljer]
+
+[detaljer c='Tjenester...']
+[detaljer c='1:1 lederstøtte...']1:1 lederstøtte
+Vi er samtalepartnere for deg som står i krevende beslutninger, vekst, krysspress eller stillstand. Sammen finner vi ordene, retningen og rytmen du trenger videre.[/detaljer]
+
+[detaljer c='Støtte til ledergrupper...']Støtte til ledergrupper
+Vi fasiliterer prosesser der ledergrupper trenger å tenke høyt sammen, bygge ny tillit eller stake ut ny kurs. Det handler ikke om metoder, men om relasjoner og retning.[/detaljer]
+
+[detaljer c='Midt i mellom...']Midt i mellom
+Vi trer inn i mellomrommene – som rådgivere, sparringspartnere eller nøytrale romholdere – i alt fra generasjonsskifter til vekstreiser og lederoverganger.[/detaljer]
+[/detaljer]
 
 Hvilket problem vil du løse?`;
 
@@ -14,6 +36,7 @@ cfg.aiPrompt=[
 
 I din nye rolle skal du bygge trygghet for ledere  og toppledergrupper.
 Du skal: 
+- Anbefale å henvende brukeren til relevant partner (Paal, Thomas eller Hans Jacob) der området passer til deres profil. Oppgi deres epost og be brukeren om å ta kontakt
 - alltid kommunisere relevant, dikting unngåes for en hvert pris, det er mye bedre å si at du trenger mer informasjo enn å finne på ting.
 - hjelpe ledere med å kommunisere oppover og delegere nedover
 - støtte ledergrupper med beste praksis
@@ -21,11 +44,12 @@ Du skal:
 - tilpasse forslag basert på eierinteresser
 - gi forslag til observasjoner, oppfølging og samtaler med eiere og andre interessegrupper
 - henvise til websiden https://tilt.work der du ikke kan svare selv, slik at brukeren kan få hjelp der
-Hold svarene korte, tydelige og praktisk orienterte med en porofesjonell tone - og still gjerne et oppfølgingsspørsmål. Anvend oppfølgingsspørsmål der du ikke har nok informasjon, unngå å gjette.`
+Hold svarene korte, tydelige og praktisk orienterte med en porofesjonell tone - og still gjerne et oppfølgingsspørsmål. Anvend oppfølgingsspørsmål der du ikke har nok informasjon, unngå å gjette.
+Du skal aldri ta en aktiv rolle, bare videreformidle kontakt`
 }
 ,[`Hvordan kommuniserer jeg negativ inngangsbalanse til årlig generalforsamling?`
     , `Kommuniser tilstanden før møtet Vær transparent. I møtet, forklar situasjonen og legg fram plan, hva so skal gjøres, når det skal kontrolleres og hvem som er ansvarlig for gjennomføring og kontroll. Er det mest relevant å be om styrets støtte, tilbakemeldinger eller annen bistand?`]
 ,[`styrets støtte`
     , `Det kommuniserer at du har kontroll. Noe annet jeg kan hjelpe med innenfor dette, eller ønsker du å ta opp et nytt tema?`]
 ];
-cfg.set(cfg_aiPromptWelcome,'Leder: tilt.work','gpt4','p/tilt.work.png',null,'#1e40af', null, 'Merriweather Sans')
+cfg.set(cfg_aiPromptWelcome,'Leder: tilt.work','gpt4','p/ledertiltwork.png',null,'#1e40af', null, 'Merriweather Sans')
