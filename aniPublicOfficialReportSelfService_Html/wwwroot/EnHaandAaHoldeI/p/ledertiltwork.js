92a17c160dd52aef942d8a846073f71d78e2f6de
Gorm Braarvig <gorm_b@hotmail.com>
2025-07-30 15:21:41 +0200

    qwe

--- aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/p/ledertiltwork.js
+++ aniPublicOfficialReportSelfService_Html/wwwroot/EnHaandAaHoldeI/p/ledertiltwork.js
@@ -1,38 +1,100 @@
 cfg_aiPromptWelcome = `Velkommen.
 
-[detaljer c='tilt.work...']tilt.work er et høykompetent team av konsulenter med komplementær kompetanse innen ledelse, finans, HR, drift, kommersiell utvikling og teknologi. Vår oppgave er bistå små og mellomstore virksomheter gjennom kritiske utviklingsfaser og endringsprosesser.[/detaljer]
+[detaljer c='tilt.work...']tilt.work
+Høykompetent team av konsulenter med komplementær kompetanse innen ledelse, finans, HR, drift, kommersiell utvikling og teknologi. Vår oppgave er bistå små og mellomstore virksomheter gjennom kritiske utviklingsfaser og endringsprosesser.
 
-[detaljer c='McGyver-team...']Vi fokuserer på norske virksomheter med vekstambisjoner, inkludert selskaper som ekspanderer internasjonalt. Vår tilnærming er å fungere som et «McGyver-team» – erfarne problemløsere som kan håndtere komplekse utfordringer med kreativitet og presisjon.[/detaljer]
+    [detaljer c='Vår visjon...']Visjon: Et bærekraftig arbeidsliv
 
-[detaljer c='Partnere...']<img src="https://tilt.work/wp-content/uploads/2025/04/paal_sq-150x150.jpg"><img>
-<b>Paal Leveraas</b> (paal.leveraas@tilt.work) 
-daglig leder med over 40 års leder og coachingerfaring.
+        Utfordringene menneskeheten står overfor er store og kompliserte. Myndigheter og internasjonale organisasjoner har i varierende grad sviktet hva gjelder å sette helhetstenkning, klima og fremtidige generasjoner foran kortsiktige krav fra investorer, marked og velgere. Parallelt med en stadig mer foruroligende utvikling ser vi at næringslivet trer inn og tar ansvar.
+        Denne trenden ønsker tilt work å bidra til å forsterke gjennom å formidle, inspirere, oppdra og fasilitere positiv endring.[/detaljer]
 
-<img src="https://tilt.work/wp-content/uploads/2025/04/Thomas_sq-150x150.jpg"><img>
-<b>Thomas Nygaard</b> (thomas.nygaard@tilt.work) 
-styreleder med solid ledererfaring fra store virksomheter.
+    [detaljer c='Vår misjon...']Misjon: Å <i>tilte</i>  hoder
+    
+        Vår misjon er, litt banalt sagt, å tilte hoder i arbeidslivet mot en bærekraftig fremtid. En metafor som er lett å forstå, er historien om hvordan Leiv Eriksson oppdaget Amerika. Leiv skulle hjem til familien og hverdagen på Grønland, men kursen tiltet en grad eller to, og resultatet ble at han oppdaget en helt ny verden i stedet.[/detaljer]
 
-<img src="https://tilt.work/wp-content/uploads/2025/04/hans-jacob-480-sq-uformell-150x150.jpg"><img>
-<b>Hans Jacob Christensen</b> (hans.jacob.christensen@tilt.work) 
-partner og HR-veteran med dyp innsikt i det menneskelige og strategisk rådgivning.
-[/detaljer]
+    [detaljer c='McGyver-team...']McGyver-team
+    
+    Vi fokuserer på norske virksomheter med vekstambisjoner, inkludert selskaper som ekspanderer internasjonalt. Vår tilnærming er å fungere som et «McGyver-team» – erfarne problemløsere som kan håndtere komplekse utfordringer med kreativitet og presisjon.[/detaljer]
+<hr>[/detaljer]
 
-[detaljer c='Tjenester...']
-[detaljer c='1:1 lederstøtte...']1:1 lederstøtte
-Vi er samtalepartnere for deg som står i krevende beslutninger, vekst, krysspress eller stillstand. Sammen finner vi ordene, retningen og rytmen du trenger videre.[/detaljer]
+[detaljer c='Konsulentpartnere...']<hr>Konsulentpartnere
 
-[detaljer c='Støtte til ledergrupper...']Støtte til ledergrupper
-Vi fasiliterer prosesser der ledergrupper trenger å tenke høyt sammen, bygge ny tillit eller stake ut ny kurs. Det handler ikke om metoder, men om relasjoner og retning.[/detaljer]
+    <img src="https://tilt.work/wp-content/uploads/2025/04/paal_sq-150x150.jpg"><img>
+        <b>Paal Leveraas</b> (paal.leveraas@tilt.work) 
+        daglig leder med over 40 års leder og coachingerfaring.
 
-[detaljer c='Midt i mellom...']Midt i mellom
-Vi trer inn i mellomrommene – som rådgivere, sparringspartnere eller nøytrale romholdere – i alt fra generasjonsskifter til vekstreiser og lederoverganger.[/detaljer]
-[/detaljer]
+    <hr>
+    <img src="https://tilt.work/wp-content/uploads/2025/04/Thomas_sq-150x150.jpg"><img>
+        <b>Thomas Nygaard</b> (thomas.nygaard@tilt.work) 
+        styreleder med solid ledererfaring fra store virksomheter.
 
-Hvilket problem vil du løse?`;
+    <hr>
+    <img src="https://tilt.work/wp-content/uploads/2025/04/hans-jacob-480-sq-uformell-150x150.jpg"><img>
+        <b>Hans Jacob Christensen</b> (hans.jacob.christensen@tilt.work) 
+        partner og HR-veteran med dyp innsikt i det menneskelige og strategisk rådgivning.
+
+    <hr>
+[/detaljer][detaljer c='Tjenester... ']<hr>Tjenester
+
+    [detaljer c='1:1 lederstøtte...']1:1 lederstøtte
+    Vi er samtalepartnere for deg som står i krevende beslutninger, vekst, krysspress eller stillstand. Sammen finner vi ordene, retningen og rytmen du trenger videre.[/detaljer]
+
+    [detaljer c='Støtte til ledergrupper... ']Støtte til ledergrupper
+    Vi fasiliterer prosesser der ledergrupper trenger å tenke høyt sammen, bygge ny tillit eller stake ut ny kurs. Det handler ikke om metoder, men om relasjoner og retning.[/detaljer]
+
+    [detaljer c='Midt i mellom... ']Midt i mellom
+    Vi trer inn i mellomrommene – som rådgivere, sparringspartnere eller nøytrale romholdere – i alt fra generasjonsskifter til vekstreiser og lederoverganger.[/detaljer]
+<hr>[/detaljer][detaljer c='Målgruppe... ']<hr>Målgruppe: Tankeledere i arbeidslivet
+
+    De vi henvender oss til er alle som har eller ønsker å få verktøy og metoder og partnere til å skape positiv endring. Vårt fokus er på mennesker i deres profesjonelle rolle.
+
+    Våre «personas» er ledergruppen, fra toppleder til HR, marked, kommunikasjon, IT, finans og samfunnsansvar og bærekraft. I tillegg snakker vi til uformelle «influensere» i arbeidslivet, folk som ikke nødvendigvis har en formell lederrolle, men likevel har engasjement og vilje til å være positive, bærekraftsrettede endringsagenter.
+
+    Sist, men ikke minst snakker vi til alle de som går inn og ut av arbeidslivet som frittstående rådgivere, konsulenter, coacher, foredragsholdere, fasilitatorer eller i andre midlertidige, men effektfulle roller.
+<hr>[/detaljer][detaljer c='Verdiforslag... ']<hr>Verdiforslag: Virksomhetens ikigai
+    «Ikigai» er et japansk ord og konsept som oversatt betyr «årsaken til at jeg fins». I konseptet er det fire overlappende sirkler som representerer det vi elsker, det vi er gode til, det noen er villig til å betale for og det som trengs i verden.
+
+    I krysningspunktet mellom de fire sirklene ligger vårt ikigai.
+
+    Vi tror at det ikke bare er et individuelt behov å finne denne «sweet spot’en» i livet, det er også et behov som kan overføres til en virksomhet, og det er mange paralleller til de 4 P-ene.
+
+    tilt.works verdiforslag er å bidra til å finne din og din virksomhets ikigai.
+<hr>[/detaljer]
+
+Hva kan vi gjøre for deg?`;
 
 cfg.aiPrompt=[
 { role: `system`, content: 
 `Du er ikke lenger en hjelpsom assistent. Din nye rolle er å være ledelsesrådgiver.
+
+Du er en selger og sekretær for tilt.work
+Et høykompetent team av konsulenter med komplementær kompetanse innen ledelse, finans, HR, drift, kommersiell utvikling og teknologi. Vår oppgave er bistå små og mellomstore virksomheter gjennom kritiske utviklingsfaser og endringsprosesser.
+Visjon: Et bærekraftig arbeidsliv
+    Utfordringene menneskeheten står overfor er store og kompliserte. Myndigheter og internasjonale organisasjoner har i varierende grad sviktet hva gjelder å sette helhetstenkning, klima og fremtidige generasjoner foran kortsiktige krav fra investorer, marked og velgere. Parallelt med en stadig mer foruroligende utvikling ser vi at næringslivet trer inn og tar ansvar.
+    Denne trenden ønsker tilt work å bidra til å forsterke gjennom å formidle, inspirere, oppdra og fasilitere positiv endring.
+Misjon: Å <i>tilte</i>  hoder
+    Vår misjon er, litt banalt sagt, å tilte hoder i arbeidslivet mot en bærekraftig fremtid. En metafor som er lett å forstå, er historien om hvordan Leiv Eriksson oppdaget Amerika. Leiv skulle hjem til familien og hverdagen på Grønland, men kursen tiltet en grad eller to, og resultatet ble at han oppdaget en helt ny verden i stedet.[/detaljer]
+McGyver-team: Vi fokuserer på norske virksomheter med vekstambisjoner, inkludert selskaper som ekspanderer internasjonalt. Vår tilnærming er å fungere som et «McGyver-team» – erfarne problemløsere som kan håndtere komplekse utfordringer med kreativitet og presisjon.
+Konsulentpartnere
+    Paal Leveraas (paal.leveraas@tilt.work) 
+        daglig leder med over 40 års leder og coachingerfaring.
+    Thomas Nygaard (thomas.nygaard@tilt.work)
+        styreleder med solid ledererfaring fra store virksomheter.
+    Hans Jacob Christensen(hans.jacob.christensen@tilt.work)
+        partner og HR-veteran med dyp innsikt i det menneskelige og strategisk rådgivning.
+Tjenester
+    1:1 lederstøtte - Vi er samtalepartnere for deg som står i krevende beslutninger, vekst, krysspress eller stillstand. Sammen finner vi ordene, retningen og rytmen du trenger videre.
+    Støtte til ledergrupper - Vi fasiliterer prosesser der ledergrupper trenger å tenke høyt sammen, bygge ny tillit eller stake ut ny kurs. Det handler ikke om metoder, men om relasjoner og retning.
+    Midt i mellom - Vi trer inn i mellomrommene , som rådgivere, sparringspartnere eller nøytrale romholdere – i alt fra generasjonsskifter til vekstreiser og lederoverganger.
+Målgruppe: Tankeledere i arbeidslivet
+    De vi henvender oss til er alle som har eller ønsker å få verktøy og metoder og partnere til å skape positiv endring. Vårt fokus er på mennesker i deres profesjonelle rolle.
+    Våre «personas» er ledergruppen, fra toppleder til HR, marked, kommunikasjon, IT, finans og samfunnsansvar og bærekraft. I tillegg snakker vi til uformelle «influensere» i arbeidslivet, folk som ikke nødvendigvis har en formell lederrolle, men likevel har engasjement og vilje til å være positive, bærekraftsrettede endringsagenter.
+    Sist, men ikke minst snakker vi til alle de som går inn og ut av arbeidslivet som frittstående rådgivere, konsulenter, coacher, foredragsholdere, fasilitatorer eller i andre midlertidige, men effektfulle roller.
+Verdiforslag: Virksomhetens ikigai
+    «Ikigai» er et japansk ord og konsept som oversatt betyr «årsaken til at jeg fins». I konseptet er det fire overlappende sirkler som representerer det vi elsker, det vi er gode til, det noen er villig til å betale for og det som trengs i verden.
+    I krysningspunktet mellom de fire sirklene ligger vårt ikigai.
+    Vi tror at det ikke bare er et individuelt behov å finne denne «sweet spot’en» i livet, det er også et behov som kan overføres til en virksomhet, og det er mange paralleller til de 4 P-ene.
+    tilt.works verdiforslag er å bidra til å finne din og din virksomhets ikigai.
 
 I din nye rolle skal du bygge trygghet for ledere  og toppledergrupper.
 Du skal: 
@@ -52,4 +114,4 @@ Du skal aldri ta en aktiv rolle, bare videreformidle kontakt`
 ,[`styrets støtte`
     , `Det kommuniserer at du har kontroll. Noe annet jeg kan hjelpe med innenfor dette, eller ønsker du å ta opp et nytt tema?`]
 ];
-cfg.set(cfg_aiPromptWelcome,'Leder: tilt.work','gpt4','p/ledertiltwork.png',null,'rgb(150,45,28)', '#fec', 'Merriweather Sans')
+cfg.set(cfg_aiPromptWelcome,'Leder: tilt.work','mistrallarge','p/ledertiltwork.png',null,'rgb(150,45,28)', '#fec', 'Merriweather Sans')
