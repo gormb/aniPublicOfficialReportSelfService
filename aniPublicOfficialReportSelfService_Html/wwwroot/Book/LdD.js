6434f8f82efa89adce70c9ed9b280fd8fb63787f
Gorm Braarvig <your@email.com>
2026-07-29 12:58:38 +0200

    Refactor code structure for improved readability and maintainability

--- aniPublicOfficialReportSelfService_Html/wwwroot/Book/LdD.js
+++ aniPublicOfficialReportSelfService_Html/wwwroot/Book/LdD.js
@@ -1,47 +1,33 @@
-import * as pdfjsLib from 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.min.mjs';
-pdfjsLib.GlobalWorkerOptions.workerSrc='https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.mjs';
+import * as _cBookJLib from 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.min.mjs';
+_cBookJLib.GlobalWorkerOptions.workerSrc='https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.mjs';
 
-const canvas = document.getElementById("_cBook");
-const ctx = canvas.getContext("2d");
-let pdfPromise;
-let pdfSrc;
-
-let renderTask;
-
-async function cShow(src, height) {
-    if (src !== pdfSrc) {
-        pdfSrc = src;
-        pdfPromise = pdfjsLib.getDocument(src).promise;
+let cBook={ctx:null,pdf:null,page:null,pn:0,viewport:null,scale:null,view:null,pdfPromise:null,renderTask:null
+    ,Source:async function(src,render) {
+        cBook.ctx = cBook.ctx || _cBook.getContext("2d");
+        cBook.pdfPromise = cBook.pdfPromise || _cBookJLib.getDocument(src).promise;
+        cBook.pdf = cBook.pdf || await cBook.pdfPromise;
+        if (render) await cBook.Page(cBook.pn, true);
     }
-    const pdf = await pdfPromise;
-    const page = await pdf.getPage(1);
+    ,Page:async function(pageNo,render) {
+        if(pageNo<1) pageNo=1;
+        else if(pageNo>cBook.pdf.numPages) pageNo=cBook.pdf.numPages;
+        if (cBook.pn !== pageNo) {
+            cBook.pn = pageNo;
+            cBook.page = await cBook.pdf.getPage(pageNo);
+        }
+        if (render) await cBook.Width(window.innerWidth*2, true);
+    }
+    ,Width:async function(width,doRender) {
+        cBook.viewport = cBook.page.getViewport({scale:1});
+        cBook.scale = width / cBook.viewport.width;
+        cBook.view = cBook.page.getViewport({ scale: cBook.scale });
+        if (doRender) await cBook.Render();
+    }
+    ,Render:async function() {
+        if (cBook.renderTask) cBook.renderTask.cancel();
+        cBook.renderTask = cBook.page.render({canvasContext: cBook.ctx, viewport: cBook.view});
+    }
+    ,DoShow:async (src, pageNo, width)=>await cBook.Source(src,true)
+};
 
-    const viewport = page.getViewport({
-        scale: 1
-    });
-
-    const scale = height / viewport.height;
-
-    const view = page.getViewport({
-        scale: scale
-    });
-
-    // canvas.width = view.width;
-    // canvas.height = view.height;
-
-    if (renderTask) renderTask.cancel();
-
-    renderTask = page.render({
-        canvasContext: ctx,
-        viewport: view
-    });
-
-    // try {
-    //     await renderTask.promise;
-    // } catch (error) {
-    //     if (error?.name !== "RenderingCancelledException")
-    //         throw error;
-    // }
-}
-
-pdfRendererReadyResolve(cShow);
+window.cBook=cBook;
